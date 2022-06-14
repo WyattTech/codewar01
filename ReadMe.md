edb095818a74bd4f7d37cf2055c24a0e77ae2e77
April Wyatt <aprilwyatttech@gmail.com>
2022-06-14 18:47:10 -0500

    first commit

---
+++ ReadMe.md
@@ -0,0 +1,3 @@
+#JavaScript Practice
+### Learning Log for Code Wars
+#### The objective is to document my understanding and growth of JavaScript daily
---
+++ codewar.js
@@ -0,0 +1,6 @@
+function numberToString(num) {
+    // Return a string of the number here!
+    num = 67;
+    let str = num.toString(); //String Method to convert numbers to strings: toString()
+    return str
+  }
